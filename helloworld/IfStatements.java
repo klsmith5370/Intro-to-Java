a947b69efd78cf0c51dc22b72413f7314d3c6b7f
Kourtney Smith <klsmith5370@gmail.com>
2024-11-25 11:47:14 -0500

    Added more examples of if conditionals

--- helloworld/IfStatements.java
+++ helloworld/IfStatements.java
@@ -12,10 +12,35 @@ public class IfStatements {
         // If-else statement 
         if (x == 10) {
             System.out.println("Value of X is 10");
-        } else if (x > 10) {
+        } else if (x < 10) {
             System.out.println("Value of X is less than 10");
         } else {
             System.out.println("Value of X is more than 10");
         }
+
+        int y = 50;
+        // Nested if-else example
+        if (x == 100) {
+            if (y > 30) {
+                System.out.println("X is 100 and Y is more than 30");
+            }
+        }
+
+        // Switch Example
+        switch (x) {
+            case 50:
+                System.out.println("X is 50");
+                break;
+            case 60:
+                System.out.println("X is 60");
+                break;
+            case 100:
+                System.out.println("X is 100");
+                break;
+            default:
+                System.out.println("X has a different value");
+        }
+
+
     }
 }
