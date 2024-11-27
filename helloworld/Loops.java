236f7e5ade045d1bdf567d9d29937e49c46eec95
Kourtney Smith <klsmith5370@gmail.com>
2024-11-26 19:08:51 -0500

    Added loop examples

--- helloworld/Loops.java
+++ helloworld/Loops.java
@@ -1,5 +1,29 @@
 package helloworld;
 
 public class Loops {
-    
+    public static void main(String arg[]) {
+        // Example of for loop
+        // (initialization step of loop, boolean expression, the update statement (update any loop control variables)) 
+        System.out.println("Example of for loop");
+        for (int x = 10; x < 20; x = x+1) { 
+            System.out.println(x);
+        
+        }
+
+        // Example of while loop
+        System.out.println("Example of while loop");
+        int y = 10;
+
+        while (y < 50) {
+            System.out.println(y);
+            y++;
+        }
+
+        // Example of do...while loop
+        System.out.println("Example of do...while loop");
+        do {
+            System.out.println(y);
+            y++;
+        } while(y < 50);
+    }
 }
